a0267a6b22f1dd937f9a2b489755036e4fdd271a
VINCENTLE <vle3@uco.edu>
2021-08-31 23:21:52 -0500

    complete implementation of Triangle Drawing

--- view/MenuScreen.java
+++ view/MenuScreen.java
@@ -39,6 +39,15 @@ public class MenuScreen {
             window.pack();
             window.setVisible(true);
         });
+
+        drawingButton.addActionListener(event ->
+        {
+            window.getContentPane().removeAll();
+            var panel = new TriangleDrawingPanel(window);
+            panel.init();
+            window.pack();
+            window.revalidate();
+        });
     }
 
 }
